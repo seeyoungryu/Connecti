d9fbf974e42ce3f12201d61708f9f2f6d3add79c
seeyoungryu <seayeux@gmail.com>
2024-10-15 18:57:32 +0900

    init commit

---
+++ src/main/java/com/seeyoungryu/connecti/ConnectiApplication.java
@@ -0,0 +1,13 @@
+package com.seeyoungryu.connecti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seeyoungryu/connecti/ConnectiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeyoungryu.connecti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
