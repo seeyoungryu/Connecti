a6bed4534a8df8418eb875488f36b5792ace8911
seeyoungryu <seayeux@gmail.com>
2024-10-21 20:37:24 +0900

    join() 메서드 반환타입 변경

--- src/main/java/com/seeyoungryu/connecti/service/UserService.java
+++ src/main/java/com/seeyoungryu/connecti/service/UserService.java
@@ -1,12 +1,13 @@
 package com.seeyoungryu.connecti.service;
 
+import com.seeyoungryu.connecti.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     //Todo : implement
-    public void join() {
-
+    public User join() { //반환 -> User 객체를 반환하도록 함
+        return new User();
     }
 }
