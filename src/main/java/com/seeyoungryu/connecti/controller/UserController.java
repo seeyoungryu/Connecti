5e07ebcdf731eb13eb9b08ede4c92724d9e18980
seeyoungryu <seayeux@gmail.com>
2024-10-21 19:47:19 +0900

    add UserController,UserService classes

---
+++ src/main/java/com/seeyoungryu/connecti/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.seeyoungryu.connecti.controller;
+
+import com.seeyoungryu.connecti.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;  // private final -> @RequiredArgsConstructor 필요
+
+    @PostMapping
+    //Todo : implement
+    public void join() {
+        //join
+        userService.join();
+    }
+
+}
---
+++ src/main/java/com/seeyoungryu/connecti/service/UserService.java
@@ -0,0 +1,12 @@
+package com.seeyoungryu.connecti.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    //Todo : implement
+    public void join() {
+
+    }
+}
