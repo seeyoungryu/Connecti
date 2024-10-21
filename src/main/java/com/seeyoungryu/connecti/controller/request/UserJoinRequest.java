e29ea50f9791fd7bb34a20ff637211d43a7caaea
seeyoungryu <seayeux@gmail.com>
2024-10-21 18:05:21 +0900

    update UserJoinRequest.java

--- src/main/java/com/seeyoungryu/connecti/controller/request/UserJoinRequest.java
+++ src/main/java/com/seeyoungryu/connecti/controller/request/UserJoinRequest.java
@@ -1,10 +1,13 @@
 package com.seeyoungryu.connecti.controller.request;
 
 /* <TDD> 관련
-UserJoinRequest 클래스의 의도 - TDD 의 특징 관련(테스트를 먼저 작성하고 필요한 클래스나 메서드를 채워가는 방식으로 개발을 진행)
+  UserJoinRequest 클래스의 의도 -> 회원가입시 RequestBody 로 데이터 받아올 때 사용할 클래스 용도
 - 데이터 전달 객체 (DTO): UserJoinRequest는 주로 사용자로부터 전달받은 데이터(회원가입 요청의 사용자명, 비밀번호 등)를 캡슐화하는 DTO 역할을 함.
 - TDD에서의 사용: 테스트에서 사용자 요청 데이터를 전달하는 형식을 정의했기 때문에, 이를 처리할 클래스를 만들어 테스트가 올바르게 작동하도록 하기 위함.
  */
 
 public class UserJoinRequest {
+
+    private String userName;
+    private String password;
 }
