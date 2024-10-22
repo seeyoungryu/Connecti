ec55c80d1549b753e0767ebac8b9eab54936b7a2
seeyoungryu <seayeux@gmail.com>
2024-10-22 15:46:49 +0900

    회원가입/실패 테스트 모킹 추가 완료

--- src/test/java/com/seeyoungryu/connecti/controller/UserControllerTest.java
+++ src/test/java/com/seeyoungryu/connecti/controller/UserControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //@AutoConfigureMockMvc 어노테이션 (현재 API 형태의 컨트롤러를 테스트하는 중임)
-//ㄴ> 웹 서버를 띄우지 않고도 컨트롤러의 API를 테스트할 수 있음->API 요청을 "모의"로 보내고 그 응답을 검증하는 테스트 가능
+// : 웹 서버를 띄우지 않고도 컨트롤러의 API를 테스트할 수 있음->API 요청을 "모의"로 보내고 그 응답을 검증하는 테스트 가능
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -43,16 +43,13 @@ public class UserControllerTest {
         String username = "testuser1";
         String password = "password1";
 
-        //@Todo : add mocking
+        // * add mocking *
         //when(userService.join().thenReturn(mock(User.class)) > 불가 (동작이 완료되지 않은 상태에서 메서드 체이닝 방식으로 잘못된 호출을 시도)
-
         User mockUser = mock(User.class);
         //User 클래스의 가짜 객체를 생성해 mockUser로 설정함.
         when(userService.join()).thenReturn(mockUser);
         //when(userService.join()).thenReturn(mockUser);: userService.join()이 호출되면 mockUser를 반환하도록 설정함.
-        //이렇게 수정하면 userService.join() 메서드가 호출될 때, 실제로 new User()를 반환하는 것이 아니라 미리 만들어둔 mockUser 객체를 반환하게 되어, 테스트에서 원하는 가짜 동작을 정의할 수 있음.
-
-
+        //ㄴ> userService.join() 메서드가 호출될 때, 실제로 new User()를 반환하는 것이 아니라 미리 만들어둔 mockUser 객체를 반환하게 되어, 테스트에서 원하는 가짜 동작을 정의할 수 있음.
 
         mockMvc.perform(post("/api/v1/users/join")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -67,7 +64,10 @@ public class UserControllerTest {
         String username = "testuser1";
         String password = "password1";
 
-        //@Todo mocking 필요
+        // * add mocking *
+        User mockUser = mock(User.class);
+        when(userService.join()).thenThrow(new RuntimeException());
+
 
         mockMvc.perform(post("/api/v1/users/join")
                         .contentType(MediaType.APPLICATION_JSON)
