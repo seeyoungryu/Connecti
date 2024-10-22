9f9d78468de09423d47ffb48554cf16384e9dbdd
seeyoungryu <seayeux@gmail.com>
2024-10-22 17:24:07 +0900

    UserService.java의 join 로직 수정 - 회원가입 시 db 저장 처리를 위한 UserEntityRepository.java 생성

---
+++ src/main/java/com/seeyoungryu/connecti/repository/UserEntityRepository.java
@@ -0,0 +1,6 @@
+package com.seeyoungryu.connecti.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserEntityRepository extends JpaRepository {
+}
--- src/main/java/com/seeyoungryu/connecti/service/UserService.java
+++ src/main/java/com/seeyoungryu/connecti/service/UserService.java
@@ -7,7 +7,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     //Todo : implement
-    public User join(String username, String password) { //반환 -> User 객체를 반환하도록 함
+    public User join(String username, String password) {
+        //1. 입력한 username 으로 이미 가입된 user 가 있는지
+        //2. 없으면 -> 회원가입 진행 (user를 DB에 등록)
+
+
+        //반환 -> User 객체를 반환하도록 함
         return new User();
     }
 
