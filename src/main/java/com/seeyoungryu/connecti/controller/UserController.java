ace87c6fe9a8d12a80f5ef5ae1ec7af1a2e4bafd
seeyoungryu <seayeux@gmail.com>
2024-10-28 20:16:18 +0900

    update response 일부 구현 (성공)

--- src/main/java/com/seeyoungryu/connecti/controller/UserController.java
+++ src/main/java/com/seeyoungryu/connecti/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.seeyoungryu.connecti.controller;
 
 import com.seeyoungryu.connecti.controller.request.UserJoinRequest;
+import com.seeyoungryu.connecti.controller.response.Response;
 import com.seeyoungryu.connecti.controller.response.UserJoinResponse;
 import com.seeyoungryu.connecti.model.User;
 import com.seeyoungryu.connecti.service.UserService;
@@ -19,9 +20,10 @@ public class UserController {
 
     @PostMapping("/join")
     //Todo : implement
-    public void join(@RequestBody UserJoinRequest request) {
-        //join
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
         User user = userService.join(request.getUserName(), request.getPassword()); //바디로 받아온값을 실제 메서드에 넘겨줄 수 있도록
-        UserJoinResponse response = UserJoinResponse.fromUser(user);
+        return Response.success(UserJoinResponse.fromUser(user));
+//        UserJoinResponse response = UserJoinResponse.fromUser(user);
+//        return Response.success(response);
     }
 }
--- src/main/java/com/seeyoungryu/connecti/controller/response/Response.java
+++ src/main/java/com/seeyoungryu/connecti/controller/response/Response.java
@@ -1,4 +1,19 @@
 package com.seeyoungryu.connecti.controller.response;
 
-public class Response {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Response<T> {
+    private String resultCode;
+    private T result;
+
+    public static Response<Void> error(String errorCord) {
+        return new Response<>(errorCord, null);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response<>("SUCCESS", null);  //하드코딩
+    }
 }
