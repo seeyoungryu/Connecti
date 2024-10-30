54b81052a6e37a7de62fb2165f54f8870f522ea8
seeyoungryu <seayeux@gmail.com>
2024-10-30 18:38:57 +0900

    update - dto(request) 주석 정리

--- src/main/java/com/seeyoungryu/connecti/controller/request/UserLoginRequest.java
+++ src/main/java/com/seeyoungryu/connecti/controller/request/UserLoginRequest.java
@@ -3,6 +3,7 @@ package com.seeyoungryu.connecti.controller.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+//DTO(데이터 전송 객체) 클래스
 @AllArgsConstructor
 @Getter
 public class UserLoginRequest {
