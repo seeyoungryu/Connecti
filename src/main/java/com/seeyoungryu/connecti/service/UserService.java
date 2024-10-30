1da5fbfae63513dd1e8755bcc0c71ec6201ce859
seeyoungryu <seayeux@gmail.com>
2024-10-30 20:48:52 +0900

    update - UserService.java 주석 정리

--- src/main/java/com/seeyoungryu/connecti/service/UserService.java
+++ src/main/java/com/seeyoungryu/connecti/service/UserService.java
@@ -16,17 +16,16 @@ public class UserService {
 
     private final UserEntityRepository userEntityRepository;
     private final PasswordEncoder encoder; // Bean 받아오기
+    // final 필드 -> @RequiredArgsConstructor 붙임
 
 
     /*
     회원가입
      */
-
-    @Transactional //익셉션 발생시 엔티티 세이브 하는 부분이 롤백됨 (데이터에 유저가 저장 안됨)
+    @Transactional
     public User join(String userName, String password) {
-        //     < 1. 입력한 username 으로 이미 가입된 user 가 있는지 확인 >
+        //1. 입력한 username 으로 이미 가입된 user 가 있는지 확인
         //(태스트 코드용 기본 코드 : Optional<UserEntity> userEntity = userEntityRepository.findByUserName(userName);)
-        //(실제구현)
         userEntityRepository.findByUserName(userName).ifPresent(it -> {   // * it(userEntity라고 해도 됨): 람다식(parameter -> {code})의 매개변수 이름을 간단히 표현
             throw new ConnectiApplicationException(ErrorCode.DUPLICATE_USER_NAME, String.format("%s is duplicated", userName));
         });
@@ -37,16 +36,15 @@ public class UserService {
 
         throw new ConnectiApplicationException(ErrorCode.DUPLICATE_USER_NAME, String.format("%s is duplicated", userName));
         //반환 -> User 객체를 반환하도록 함
-//        return User.fromEntity(userEntity);
+        //return User.fromEntity(userEntity);
     }
 
 
     /*
     로그인
      */
-
-    //Todo : implement
-    public String login(String userName, String password) { //반환 -> JWT사용할 것이므로 암호화된 문자열을 반환하는 메서드로 처리해야함 * -> 반환값 : String
+    @Transactional
+    public String login(String userName, String password) { //반환값 -> JWT사용할 것이므로 암호화된 문자열을 반환하는 메서드로 처리해야함 * -> String
 
         //1. 회원가입 여부 확인
         UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new ConnectiApplicationException(ErrorCode.DUPLICATE_USER_NAME, ""));
@@ -56,14 +54,25 @@ public class UserService {
             throw new ConnectiApplicationException(ErrorCode.DUPLICATE_USER_NAME, "");
         }
 
-        //3. 토큰 생성
-
+        //3. 토큰 생성 @Todo
         return "";
     }
 }
 
+
+
+
+
 /*
-참고 람다식 -> ::
+( @Todo 이 주석 정리 필요함
+
+
+람다식 :  (parameter) -> { code } 형식
+참고 람다식 -> :: (참조형)
 //UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(ConnectiApplicationException::new);
 - lamda -> .orElseThrow(() -> new ConnectiApplicationException());)
+
+
+- @Transactional : 익셉션 발생시 엔티티 세이브 하는 부분이 롤백됨 (작업 실패의 경우 롤백하도록 설정->데이터 무결성 보장)
+
  */
