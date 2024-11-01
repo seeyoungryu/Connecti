02eed0760625c251dff8bab456fdba79d1c0c171
seeyoungryu <seayeux@gmail.com>
2024-11-01 15:45:09 +0900

    update - service 레이어 테스트 코드에 에러 메세지도 검증하는 코드 추가, 통과함

--- src/test/java/com/seeyoungryu/connecti/service/UserServiceTest.java
+++ src/test/java/com/seeyoungryu/connecti/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.seeyoungryu.connecti.service;
 
 import com.seeyoungryu.connecti.exception.ConnectiApplicationException;
+import com.seeyoungryu.connecti.exception.ErrorCode;
 import com.seeyoungryu.connecti.fixture.UserEntityFixture;
 import com.seeyoungryu.connecti.model.entity.UserEntity;
 import com.seeyoungryu.connecti.repository.UserEntityRepository;
@@ -45,7 +46,7 @@ public class UserServiceTest {
         String password = "password";
 
         //기존 mock 대신 Fixture 사용
-        UserEntity fixture = UserEntityFixture.get(username, password);
+        UserEntity fixture = UserEntityFixture.get(1L, username, password);
 
 
         //Mocking (mock 객체 반환을 위함)
@@ -83,7 +84,7 @@ public class UserServiceTest {
         //Mocking 을 통해 -> mock(UserEntity.class) ~ 가짜 UserEntity 객체가 반환됨 ~ 실제 db와는 상관없이 UserEntity의 기능을 시뮬레이션 하는 객체 (실제 객체와 동일한 구조와 타입을 가지고 있으므로, 테스트 코드가 실제 상황과 비슷하게 작동하는지 확인할 수 있음)
 
 
-        Assertions.assertThrows(ConnectiApplicationException.class, () -> userService.join(username, password));
+        //Assertions.assertThrows(ConnectiApplicationException.class, () -> userService.join(username, password));         @Todo : 주석처리 코드 = 에러코드 검증까지 추가하기 전 코드임
         //join 시, 이미 회원가입한 유저가 있으므로, 에러를 던져줘야(throw해야함) -> 던져준 에러(exeption)로 컨트롤러 단에서 에러를 처리 할 수 있도록!
         //userService.join이 중복 사용자 예외(ConnectiApplicationException)를 던져야만 성공 (Assertions.assertThrows를 통해 userService.join 메서드를 호출할 때 ConnectiApplicationException 예외가 발생하는지 확인)
 
@@ -92,7 +93,8 @@ public class UserServiceTest {
         userService.join 메서드를 호출했을 때 중복 사용자 예외가 발생하는지를 검증하는 구조
         따라서 join 메서드 실행 중 중복된 사용자명이 확인되면, ConnectiApplicationException 예외를 던지고 테스트가 성공함.
          */
-
+        ConnectiApplicationException e = Assertions.assertThrows(ConnectiApplicationException.class, () -> userService.join(username, password));
+        Assertions.assertEquals(ErrorCode.DUPLICATE_USER_NAME, e.getErrorCode());     //기존 테스트 코드에 에러코드도 잘 나오는지 추가한 코드임
 
     }
 
@@ -114,14 +116,24 @@ public class UserServiceTest {
         String password = "password";
 
         //mock 대신에 fixture 사용
-        UserEntity fixture = UserEntityFixture.get(userName, password);// -> get -> 테스트용 가짜 userentity 가 리턴됨.
+        UserEntity fixture = UserEntityFixture.get(1L, userName, password);// -> get -> 테스트용 가짜 userentity 가 리턴됨.
 
         //mocking
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(encoder.matches(password, fixture.getPassword())).thenReturn(true); // @Todo: 로그인 성공 테스트 실패 후 모킹 추가 (추가한 이유, 흐름?)
+
         Assertions.assertDoesNotThrow(() -> userService.login(userName, password));
 
 
+        /* <  when(encoder.matches(password, fixture.getPassword())).thenReturn(true); >
+        해당 모킹 코드를 추가한 이유 : encoder.matches(password, fixture.getPassword()) 메서드의 호출 결과를 Mock으로 설정해 주어야 하기 때문
+        encoder.matches(...)는 실제 비밀번호와 인코딩된 비밀번호를 비교하는 메서드인데, 테스트에서 실제로 암호화된 비밀번호와 일치하는지 확인할 필요는 없으나,
+        But! 테스트에서 Mocking을 통해 encoder.matches(...)가 호출될 때마다 true를 반환하도록 설정하여 비밀번호가 일치하는 상황을 흉내내서 가정함.
+        이로 인해 userService.login(userName, password) 메서드가 encoder.matches(...)의 결과에 의존하지 않고 테스트에서 예상한 대로 성공하도록 만듦
+        -> 이 줄을 추가하지 않으면 encoder.matches(...) 호출 시 실제 비밀번호를 확인하는 과정을 거치지 않기 때문에, 테스트가 실패하게 됨
+         */
     }
+
 
     /*
     로그인 테스트 (실패 - 미가입)
@@ -134,7 +146,9 @@ public class UserServiceTest {
 
         //mocking
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
-        Assertions.assertThrows(ConnectiApplicationException.class, () -> userService.login(userName, password));
+
+        ConnectiApplicationException e = Assertions.assertThrows(ConnectiApplicationException.class, () -> userService.login(userName, password));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, e.getErrorCode());
 
     }
 
@@ -148,12 +162,13 @@ public class UserServiceTest {
         String password = "password";
         String wrongPassword = "wrongPassword"; //로그인 실패
 
-        UserEntity fixture = UserEntityFixture.get(userName, password);
+        UserEntity fixture = UserEntityFixture.get(1L, userName, password);
 
         //mocking
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
-        Assertions.assertThrows(ConnectiApplicationException.class, () -> userService.login(userName, wrongPassword));  // 잘못된 패스워드
 
+        ConnectiApplicationException e = Assertions.assertThrows(ConnectiApplicationException.class, () -> userService.login(userName, wrongPassword));  // 잘못된 패스워드
+        Assertions.assertEquals(ErrorCode.INVALID_PASSWORD, e.getErrorCode());
 
     }
 
