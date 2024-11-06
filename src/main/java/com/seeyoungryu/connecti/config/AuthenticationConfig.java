39ee2f5faeaec6e685dd9163f1edd8b7d0b3fef9
seeyoungryu <seayeux@gmail.com>
2024-11-06 19:17:50 +0900

    런타임 에러 수정 - config클래스 통합, application.properties 설정값 수정 (postgreSQL)

--- src/main/java/com/seeyoungryu/connecti/config/AuthenticationConfig.java
+++ src/main/java/com/seeyoungryu/connecti/config/AuthenticationConfig.java
@@ -1,39 +1,39 @@
-package com.seeyoungryu.connecti.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-//Spring Security 설정 클래스 (접근권한,세션정책 관리)
-@Configuration //스프링의 <설정>파일로 인식하도록 함
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class AuthenticationConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()  // 특정 경로는 접근 허용
-                        .anyRequest().authenticated()  // 나머지는 인증 필요
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 세션을 사용하지 않도록 설정 (이렇게 설정하면 세션을 사용하지 않는 방식(주로 JWT와 같은 토큰 인증 방식에서 사용)으로 보안을 구성할 수 있음
-                );
-
-        return http.build();
-    }
-}
-
-
-//람다 -> 참조식
-// 람다: .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
-// 참조식으로 변경: .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
-
-//@Bean -> 반환되는 SecurityFilterChain 객체를 Spring의 빈으로 등록하여 사용할 수 있게 함.
+//package com.seeyoungryu.connecti.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+////Spring Security 설정 클래스 (접근권한,세션정책 관리)
+//@Configuration //스프링의 <설정>파일로 인식하도록 함
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class AuthenticationConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()  // 특정 경로는 접근 허용
+//                        .anyRequest().authenticated()  // 나머지는 인증 필요
+//                )
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 세션을 사용하지 않도록 설정 (이렇게 설정하면 세션을 사용하지 않는 방식(주로 JWT와 같은 토큰 인증 방식에서 사용)으로 보안을 구성할 수 있음
+//                );
+//
+//        return http.build();
+//    }
+//}
+//
+//
+////람다 -> 참조식
+//// 람다: .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
+//// 참조식으로 변경: .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
+//
+////@Bean -> 반환되는 SecurityFilterChain 객체를 Spring의 빈으로 등록하여 사용할 수 있게 함.
--- src/main/java/com/seeyoungryu/connecti/config/SecurityConfig.java
+++ src/main/java/com/seeyoungryu/connecti/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,43 +23,29 @@ public class SecurityConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-
-    /*
-    SecurityFilterChain을 빈으로 직접 정의하여 보안 설정을 구성
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                // CSRF 비활성화
-                // .csrf(csrf -> csrf.disable())  : 메서드의 인자로 전달되어 CSRF 설정을 끄는 작업을 수행 (람다식)
-                /* 기존에는 메서드 체이닝 방식을 씀 (csrf() 메서드를 호출한 후에, 그 결과로 반환된 객체에 대해 다시 disable()을 호출하여 체이닝 방식으로 이어서 씀)
-                   현재 사용하는 방식: csrf().disable()은 메서드 체이닝에서 disable()로 호출하지 않고, << 람다 표현식 안에서 disable() 메서드를 사용>> 하는 것이 맞음
-                */
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/users/login", "/api/v1/users/join").permitAll() // 로그인과 회원가입은 모두 접근 가능
-                        .requestMatchers("/api/v1/users/me").authenticated() // `me` 엔드포인트는 인증이 필요    //requestMatchers()로 URL 패턴을 지정
+                        .requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // 로그인과 회원가입은 모두 접근 가능
                         .anyRequest().authenticated() // 나머지 모든 요청은 인증이 필요
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않음
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
 
         return http.build();
     }
 
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
-    /*
-   AuthenticationManager 빈 등록: AuthenticationManager를 직접 빈으로 등록해 사용할 수 있도록 설정
-    */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-
-
 }
