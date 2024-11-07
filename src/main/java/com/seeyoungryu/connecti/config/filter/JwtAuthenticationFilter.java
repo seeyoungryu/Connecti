0dfc6e134db8e171fff6a4df1916c4ac8f246132
seeyoungryu <seayeux@gmail.com>
2024-11-07 13:40:25 +0900

    feat : User 회원가입/로그인 기능 완료

--- src/main/java/com/seeyoungryu/connecti/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/seeyoungryu/connecti/config/filter/JwtAuthenticationFilter.java
@@ -13,6 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
--- src/main/java/com/seeyoungryu/connecti/service/UserService.java
+++ src/main/java/com/seeyoungryu/connecti/service/UserService.java
@@ -15,15 +15,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor //final 필드
 @Service
 public class UserService implements UserDetailsService {
     private final UserEntityRepository userEntityRepository;
     private final PasswordEncoder encoder; // Bean 받아오기
-    // final 필드 -> @RequiredArgsConstructor 붙임
 
-
-    @Value("${jwt.secret-key}")  //application 설정값 자동 주입
+    @Value("${jwt.secret-key}")  //  @Value : application 설정값 자동 주입
     private String secretKey;
 
     @Value("${jwt.token.expired-time-ms}")
@@ -31,14 +29,14 @@ public class UserService implements UserDetailsService {
 
 
 
-    /* loadUserByUsername() : Spring Security의 인증을 위해 `UserDetailsService` 인터페이스를 구현하고 사용자 정보를 조회
-      ㄴ> Spring Security가 사용자를 인증할 때 호출하여 사용자 정보를 로드, 사용자의 `username`을 기반으로 `User` 정보를 반환
-     */
+  /* loadUserByUsername(String userName) 메서드 - (UserDetailsService 인터페이스의 loadUserByUsername 메서드를 구현함)
+    Spring Security가 사용자를 '인증'할 때 호출하여 사용자 정보를 로드함 (사용자의 `username`을 기반으로 `User` 정보를 조회하여 반환)
+   */
 
     @Override
     public User loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return (User) userEntityRepository.findByUserName(userName)
-                .map(User::fromEntity)
+        return userEntityRepository.findByUserName(userName)  //사용자의 `username`을 기반으로 `User` 정보를 반환
+                .map(User::fromEntity)    //이 메서드가 User 타입을 반환함
                 .orElseThrow(() -> new ConnectiApplicationException(ErrorCode.USER_NOT_FOUND, String.format("userName: %s", userName)));
     }
 
@@ -50,7 +48,7 @@ public class UserService implements UserDetailsService {
     public User join(String userName, String password) {
         //1. 입력한 username 으로 이미 가입된 user 가 있는지 확인
         //(태스트 코드용 기본 코드 : Optional<UserEntity> userEntity = userEntityRepository.findByUserName(userName);)
-        userEntityRepository.findByUserName(userName).ifPresent(it -> {   // * it(userEntity라고 해도 됨): 람다식(parameter -> {code})의 매개변수 이름을 간단히 표현
+        userEntityRepository.findByUserName(userName).ifPresent(it -> {   // * it(=userEntity): 람다식(parameter -> {code})의 매개변수 이름을 간단히 표현
             throw new ConnectiApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("userName: %s", userName));
         });
 
@@ -67,8 +65,11 @@ public class UserService implements UserDetailsService {
     /*
     로그인
      */
+    @Transactional
     public String login(String userName, String password) {
         UserDetails userDetails = loadUserByUsername(userName);
+        //Spring Security가 사용자를 '인증'하므로 loadUserByUsername 호출 -> 사용자의 `username`을 기반으로 `User` 정보를 조회하여 반환함.
+
         if (!encoder.matches(password, userDetails.getPassword())) {
             throw new ConnectiApplicationException(ErrorCode.INVALID_PASSWORD);
         }
