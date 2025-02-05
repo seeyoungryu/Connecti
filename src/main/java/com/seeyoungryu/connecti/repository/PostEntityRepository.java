597a31c74fb6e69e6924a4b1f497587b56e5895b
seeyoungryu <seayeux@gmail.com>
2025-02-05 23:21:26 +0900

    refactor: 레파지토리 코드 정리

--- src/main/java/com/seeyoungryu/connecti/repository/PostEntityRepository.java
+++ src/main/java/com/seeyoungryu/connecti/repository/PostEntityRepository.java
@@ -6,11 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface PostEntityRepository extends JpaRepository<PostEntity, Long> {   // id -> Long 타입
-    Optional<PostEntity> findById(Long postId);
-
-    Page<PostEntity> findAllByUserId(Long uerId, Pageable pageable);
+public interface PostEntityRepository extends JpaRepository<PostEntity, Long> {
+    Page<PostEntity> findAllByUserId(Long userId, Pageable pageable);
 }
