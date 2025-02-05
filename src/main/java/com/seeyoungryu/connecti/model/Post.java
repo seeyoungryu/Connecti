61d857e9d6fe231d2c2bb7290d1ca2095d0576d4
seeyoungryu <seayeux@gmail.com>
2025-02-05 21:21:07 +0900

    refactor: Post DTO 개선 및 Setter 제거
    - Timestamp → LocalDateTime 변경
    - fromEntity()에서 deletedAt 필드 null 처리 보강

--- src/main/java/com/seeyoungryu/connecti/model/Post.java
+++ src/main/java/com/seeyoungryu/connecti/model/Post.java
@@ -1,30 +1,30 @@
 package com.seeyoungryu.connecti.model;
 
 import com.seeyoungryu.connecti.model.entity.PostEntity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
-public class Post {
+@RequiredArgsConstructor
+public class Post { //DTO 클래스 (데이터 가공 후 postEntity 반환 (postEntity로 변환)
 
-    private Long id;
-    private String title;
-    private String body;
-    private Timestamp registeredAt;
-    private Timestamp updatedAt;
-    private Timestamp deletedAt;
+    private final Long id;
+    private final String title;
+    private final String body;
+    private final LocalDateTime registeredAt;
+    private final LocalDateTime updatedAt;
+    private final LocalDateTime deletedAt;
 
     public static Post fromEntity(PostEntity postEntity) {
         return new Post(
                 postEntity.getId(),
                 postEntity.getTitle(),
                 postEntity.getBody(),
-                postEntity.getRegisteredAt(),
-                postEntity.getUpdatedAt(),
-                postEntity.getDeletedAt()
+                postEntity.getRegisteredAt().toLocalDateTime(),
+                postEntity.getUpdatedAt().toLocalDateTime(),
+                postEntity.getDeletedAt() != null ? postEntity.getDeletedAt().toLocalDateTime() : null //null 체크 추가
         );
     }
 }
